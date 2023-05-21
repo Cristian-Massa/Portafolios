c99332de0c930f08410e614bd3666986cc3510c1
Cristian-Massa <cristianmassa30@gmail.com>
2023-05-21 11:32:44 -0300

    finishing projects modal

--- Server/src/Controllers/getProjectGH.js
+++ Server/src/Controllers/getProjectGH.js
@@ -4,13 +4,14 @@ const projectGets =  (req, res) => {
     axios.get("https://api.github.com/users/Cristian-Massa/repos")
         .then(response =>{
             const works = response.data.filter(element => element.description.includes("#Project") || element.description.includes("#Exercise"))
-            const filteredWorks = works.map(({id, name, html_url, language, created_at, homepage}) => ({
+            const filteredWorks = works.map(({id, name, html_url, language, created_at, homepage, description}) => ({
                 id, 
                 name, 
                 html_url, 
                 language, 
                 created_at, 
-                homepage
+                homepage,
+                description
             }))
             res.status(200).json(filteredWorks)
         })
